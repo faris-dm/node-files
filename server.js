ad27914e0b8b76dfe871414afd15ab7b9725a800
faris Naser <solonaser9@gmail.com>
2025-12-09 07:51:24 -0800

    fix: solve the error request that  say name not defined  by changeing the puush  to nessery names

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 import express from "express";
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const product = [
+  { id: 1, name: "faris" },
+  { id: 2, name: "naser" },
+  { id: 3, name: "nida" },
+];
 
 app.get("/", (req, res) => {
-  res.send("   this is a home page ");
+  res.send("  this a tender client  new post man we  solve the prolem");
+});
+app.get("/product", (req, res) => {
+  res.send(product);
 });
 
-app.get("/login", (req, res) => {
-  if (req.url === 404) {
-    res.send(" the pages  does not  found");
-  }
-  res.send(" login   to acces all the authicatecation process");
+app.post("/new", (req, res) => {
+  const newItem = {
+    id: product.length + 1,
+    name: req.body.name,
+  };
+  product.push(newItem);
+  res.send(product);
 });
-const port = 6000;
+
+// app.post("/product", (req, res) => {
+//   const added = {
+//     id: product.length + 1,
+//     name: req.body.name,
+//   };
+//   product.push(added);
+//   res.send(product);
+// });
+
+const port = 7000;
 app.listen(port, () => {
-  console.log(`http://localhost${port}`);
+  console.log(`http://localhost:${port}`);
 });
