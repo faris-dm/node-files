f744963b4ee549da0ee14d8d72b33db4b0afd120
faris Naser <solonaser9@gmail.com>
2025-12-09 03:24:55 -0800

    add a gitignore  to excluide  node_mosule

--- server.js
+++ server.js
@@ -1,70 +1,17 @@
 import express from "express";
-import Joi from "joi";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(
-    " this  is hero pages  and i am learning  post man   which is amazing"
-  );
+  res.send("   this is a home page ");
 });
 
-app.get("/contact", (req, res) => {
-  res.end(
-    "  contact as whatwver  you want  and   my idea is    to be a good dev   in  mnageing apis a nd  work flow"
-  );
+app.get("/login", (req, res) => {
+  if (req.url === 404) {
+    res.send(" the pages  does not  found");
+  }
+  res.send(" login   to acces all the authicatecation process");
 });
-
-const port = 5000;
+const port = 6000;
 app.listen(port, () => {
-  console.log(`server active on http://localhost:${port}`);
+  console.log(`http://localhost${port}`);
 });
-
-// const items = [
-//   { id: 1, name: "solo", age: 10 },
-//   { id: 2, name: "Mula", age: 20 },
-//   { id: 3, name: "amir", age: 30 },
-// ];
-
-// app.get("/", (req, res) => {
-//   res.send(" this is Hero Pages");
-// });
-
-// // app.get("/home", (req, res) => {
-// //   res.send("is is  home pages that talks me");
-// // });
-// // app.get("/login/:year/:month", (req, res) => {
-// //   res.send(req.params);
-// // });
-
-// app.post("/items", (req, res) => {
-//   const sheme = {
-//     name: Joi.string().min(3).required(),
-//   };
-//   const result = Joi.ValidationError(req.body, sheme);
-//   console.log(result);
-
-//   const items = {
-//     id: items.listen + 1,
-//     name: req.body.name,
-//   };
-
-//   items.push(items);
-//   res.send(items);
-// });
-
-// app.get("/items/:id", (req, res) => {
-//   let item = items.find((c) => c.id == parseInt(req.params.id));
-//   if (!items) res.status(404).send(" the   needed  items is not found");
-//   res.send(item);
-// });
-
-// app.get("/items/:id", (req, res) => {
-//   const hero = items.find((pro) => pro.id === parseInt(req.params.id));
-
-//   if (!hero) res.status(404).send("the page is not found");
-
-//   res.send(hero);
-// });
-
-// const port = 9000;
-// app.listen(port, () => console.log(` the port is running  in the 9000 `));
